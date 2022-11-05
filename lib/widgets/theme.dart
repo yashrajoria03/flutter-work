01025abc23eadb3ecee656062943244926fd24e8
Yash Rajoria <yashkhatri826@gmail.com>
2022-11-06 02:45:28 +0530

    final

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart'
+import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.inter().fontFamily,
         appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
+          color: Colors.white,
+          elevation: 0.0,
           iconTheme: IconThemeData(color: Colors.black),
-          titleTextStyle: TextStyle(color: Colors.black),
+          textTheme: Theme.of(context).textTheme,
         ),
       );
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
-  // static ThemeData darkTheme=>ThemeData();
 }
